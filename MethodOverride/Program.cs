fb969c21433ba0998bc239c9da87325ebc98796e
Jack Griph <jackgriph111@hotmail.com>
2025-09-15 16:22:14 +0200

    Add project files.

---
+++ MethodOverride/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MethodOverride
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+
+        static void MethodToOverride()
+        {
+            Console.WriteLine("Base Method");
+        }
+    }
+}
